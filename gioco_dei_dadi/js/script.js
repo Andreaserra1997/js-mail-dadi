10149264d0604d7e59cac4952df2ac9a1dc04204
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-17 15:47:56 +0200

    edit folder

---
+++ gioco_dei_dadi/js/script.js
@@ -0,0 +1,9 @@
+/*
+generare un numero random da 1 a 6 per l'utente e metterlo nella variabile userNumber
+generare un numero random da 1 a 6 per il computer e metterlo nella variabile computerNumber
+    SE (userNumber > computerNumber)
+        ALLORA stampa: L'utente ha vinto
+    ALTRIMENTI SE (computerNumber > userNumber)
+        ALLORA stampa: Il computer ha vinto
+    ALTRIMENTI stampa: I numeri sono uguali, pareggio!
+*/

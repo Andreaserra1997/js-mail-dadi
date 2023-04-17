9953f4cb93e8b73bf59d4ca21e2a37fc54bbfb63
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-17 16:57:41 +0200

    add if dadi

--- es_mail/js/script.js
+++ es_mail/js/script.js
@@ -9,10 +9,11 @@ creare lista mail accessibili
 const mailList = ['@gmail', '@virgilio', '@libero', '@hotmail', '@yahoo'];
 const email = prompt('Che tipo di email usi? es:@gmail');
 
-for (let i = 0; i < mailList.length; i+=email) {
-    if (mailList == true) {
+for (let i = 0; i < mailList.length; i++) {
+    console.log(mailList[i])
+    if (mailList[i] == email) {
         console.log('La mail è presente')
     } else {
-        console.log('La mail inserita non è valida')
+        // console.log('La mail inserita non è valida')
     }
 }
--- gioco_dei_dadi/js/script.js
+++ gioco_dei_dadi/js/script.js
@@ -7,3 +7,17 @@ generare un numero random da 1 a 6 per il computer e metterlo nella variabile co
         ALLORA stampa: Il computer ha vinto
     ALTRIMENTI stampa: I numeri sono uguali, pareggio!
 */
+
+const userNumber = Math.floor(Math.random() * 6) + 1
+console.log(`Il numero dell'utente è:` + userNumber)
+const computerNumber = Math.floor(Math.random() * 6) + 1
+console.log(`Il numero del computer è:` +computerNumber)
+
+
+if (userNumber > computerNumber) {
+    console.log(`L'utente ha vinto`);
+} else if (computerNumber > userNumber) {
+    console.log(`Il computer ha vinto`);
+} else {
+    console.log(`I numeri sono uguali, pareggio!`)
+}

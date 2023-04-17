aee2f7c23cc9f6bb9c09f0ddde5d498f2c4b51b9
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-17 14:31:58 +0200

    add pseudocode

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+chiedere mail all'utente e metterla nella variabile email
+creare lista mail accessibili
+    SE la email è presente nella lista mail accessibili
+     ALLORA stampa messaggio positivo
+    FINE SE
+
+generare un numero random da 1 a 6 per l'utente e metterlo nella variabile userNumber
+generare un numero random da 1 a 6 per il computer e metterlo nella variabile computerNumber
+    SE (userNumber > computerNumber)
+        ALLORA stampa: L'utente ha vinto
+    ALTRIMENTI SE (computerNumber > userNumber)
+        ALLORA stampa: Il computer ha vinto
+    ALTRIMENTI stampa: I numeri sono uguali, pareggio!
+*/

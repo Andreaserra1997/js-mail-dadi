0ea3279ea6eec6adb6a986abbb5f8bb4de108d6d
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-17 16:23:00 +0200

    add true

--- es_mail/js/script.js
+++ es_mail/js/script.js
@@ -7,9 +7,12 @@ creare lista mail accessibili
 */
 
 const mailList = ['@gmail', '@virgilio', '@libero', '@hotmail', '@yahoo'];
-
 const email = prompt('Che tipo di email usi? es:@gmail');
 
 for (let i = 0; i < mailList.length; i+=email) {
-    console.log(mailList[i])
+    if (mailList == true) {
+        console.log('La mail è presente')
+    } else {
+        console.log('La mail inserita non è valida')
+    }
 }
